741c975d2da25ccbd5d24741a8906206321799e9
Dengngut <ngut@ualberta.ca>
2025-09-24 20:13:10 -0600

    Third commit

--- lab04/CMPUT301F25zipbom/app/src/main/java/com/example/cmput301f25zipbom/Shape.java
+++ lab04/CMPUT301F25zipbom/app/src/main/java/com/example/cmput301f25zipbom/Shape.java
@@ -4,5 +4,13 @@ public abstract class Shape{
     String color = "orang";
     public int x;
     public int y;
+
+
+    public  Shape(int x , int y) {
+        this.x = x;
+        this.y = y;
     }
+
 }
+
+
